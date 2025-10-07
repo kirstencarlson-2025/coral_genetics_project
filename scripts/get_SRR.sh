78cf27fcc41e5329c3ec49c7f603263fd9a2a04b
Kirsten Carlson <kirstengcarlson@gmail.com>
2025-10-06 22:46:45 -0400

    Create get_SRR.sh

---
+++ scripts/get_SRR.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# WHAT: uses entrez-direct package to search for SRA runs using PRJNA accession number, then saves in csv file.
+# Searches for SRR numbers and saves in seperate 2bRAD and ITS2 files
+# Packages needed:
+# - entrez-direct
+
+# Set variables
+PRJNA=$1     #PRJNA accession number
+OUTPUT=$2    #output file name .csv
+
+#######
+#STEP 1
+#######
+esearch -db sra -query $PRJNA | efetch -format runinfo > $OUTPUT
+
+#######
+#STEP 2
+#######
+grep 2bRAD $OUTPUT | cut -f1 -d "," | grep SRR > 2bRAD_$OUTPUT
+grep ITS2 $OUTPUT | cut -f1 -d "," | grep SRR > ITS2_$OUTPUT
